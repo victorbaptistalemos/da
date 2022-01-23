405279c1cc2ccfa7d7cfbc4528bd3998e5b5edba
victorbaptistalemos <vbl.vix@icloud.com>
2022-01-23 06:57:42 +0000

    Funções para BackUp

--- diggy.py
+++ diggy.py
@@ -1,13 +1,51 @@
-#! /bin/python
+#! /bin/python3
 
-from json import dump, load
+from datetime import datetime
+from json import dump
+from json import load
+from os import name as os_name
 from os import system
 
+
+def current_time_str() -> str:
+    _time: datetime = datetime.now()
+    _time: str = str(_time)
+    _time: list = _time.split(' ')
+    _time: str = '-'.join(_time)
+    return _time
+
+
+def sys_clear() -> None:
+    if os_name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+
+def manage_backup(create_bkp: bool, bkp_name: str) -> [str, None]:
+    if create_bkp:
+        bkp_name: str = f'diggy_{bkp_name}.json'
+        if os_name == 'nt':
+            system(f'copy diggy.json {bkp_name}')
+        else:
+            system(f'cp diggy.json {bkp_name}')
+        return bkp_name
+    else:
+        if os_name == 'nt':
+            system(f'remove {bkp_name}')
+        else:
+            system(f'rm {bkp_name}')
+
+
+current_backup: None = None
+
 try:
-    system('clear')
     with open("diggy.json") as _:
         team: dict = load(_)  # {key: list, key: list, ...}
         new_team: dict = {}
+
+    current_backup: str = manage_backup(True, current_time_str())  # Creates a backup
+
     for key in team.keys():  # iterates on each player
         while True:
             player: list = team[key]  # [level, score, warning]
@@ -22,10 +60,10 @@ try:
                             raise ValueError
                         else:
                             new_data.append(data)
-                            system('clear')
+                            sys_clear()
                             break
                     except ValueError:
-                        system('clear')
+                        sys_clear()
                         if data is None:
                             new_data.append(value)
                             break
@@ -42,10 +80,10 @@ try:
                 else:
                     new_data.append(0)
                 new_team[key] = new_data
-                system('clear')
+                sys_clear()
                 break
             except KeyboardInterrupt:
-                system('clear')
+                sys_clear()
                 continue
     del team, key, player, name, value, data, new_data
     w7 = [key for key in new_team.keys() if new_team[key][2] >= 7]
@@ -56,8 +94,8 @@ try:
         else:
             continue
     with open("diggy.json", 'w') as _:
-        dump(new_team, _, indent=4)
+        dump(new_team, _, indent=True)
 except (EOFError, KeyboardInterrupt):
-    pass
+    manage_backup(False, current_backup)
 finally:
     system('clear')
