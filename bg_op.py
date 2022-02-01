82ee2baeeb1d8978ce11f7f7b67235e21497449d
Victor Baptista Lemos <vbl.vix@icloud.com>
2022-02-01 04:06:45 +0000

    Sorting functions alphabetically

--- bg_op.py
+++ bg_op.py
@@ -5,15 +5,8 @@ from os.path import abspath
 from os.path import dirname
 
 
-def current_time() -> str:
-    _time: datetime = datetime.now()
-    _time: str = str(_time)
-    _time: str = _time.split(' ')[0]
-    return _time
-
-
-def sys_clear() -> None:
-    system(f"{'cls' if os_name == 'nt' else 'clear'}")
+def current_date() -> str:
+    return str(datetime.now()).split(' ')[0]
 
 
 def current_path() -> str:
@@ -22,9 +15,13 @@ def current_path() -> str:
 
 def manage_backup(create_bkp: bool, bkp_name: str) -> [str, None]:
     if create_bkp:
-        path: str = current_path()
-        bkp_name: str = path + f'diggy_{bkp_name}.json'
-        system(f"{'copy' if os_name == 'nt' else 'cp'} {path}diggy.json {bkp_name}")
+        _path: str = current_path()
+        bkp_name: str = _path + f'diggy_{bkp_name}.json'
+        system(f"{'copy' if os_name == 'nt' else 'cp'} {_path}diggy.json {bkp_name}")
         return bkp_name
     else:
         system(f"{'remove' if os_name == 'nt' else 'rm'} {bkp_name}")
+
+
+def sys_clear() -> None:
+    system(f"{'cls' if os_name == 'nt' else 'clear'}")
